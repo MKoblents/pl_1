e47ded24f54a07a29ad5101d7d92292c48cadc38
MKoblents <koblentsmaria@gmail.com>
2026-09-12 13:15:19 +0300

    print_char corrected

--- lib.asm
+++ lib.asm
@@ -34,7 +34,6 @@ print_string:
 
 ; Принимает код символа и выводит его в stdout
 print_char:
-    push rdi
     sub rsp, 8
     mov [rsp], dil
     mov rax, 1
@@ -43,7 +42,6 @@ print_char:
     mov rdx, 1
     syscall
     add rsp, 8
-    pop rdi
     ret
 
 ; Переводит строку (выводит символ с кодом 0xA)
